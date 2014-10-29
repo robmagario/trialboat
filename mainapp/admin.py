81d61fb6839826f279c15d684481f0214b5c7ac5
DC <hemantsureshkumar2012@gmail.com>
2014-10-29 18:47:19 +0800

    changed customer

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from mainapp.models import Product
+from mainapp.models import Customer
 
 admin.site.register(Product)
+admin.site.register(Customer)
 # Register your models here.
--- mainapp/models.py
+++ mainapp/models.py
@@ -4,6 +4,13 @@ class Product(models.Model):
     description = models.CharField(max_length=200)
     weight = models.IntegerField(default=0)
 
+class Customer(models.Model):
+    firstname =models.CharField(max_length=100)
+    familyname = models.CharField(max_length=100)
+    address=models.CharField(max_length=200)
+    zipcode = models.IntegerField(default=0)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
 
 
 # Create your models here.
