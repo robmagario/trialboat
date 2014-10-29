7144ff64b381c83184fc376f38b3186adb397e02
Robson HayashidaE <robson@magario.com>
2014-10-29 18:41:06 +0800

    this version works fine.

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Product(models.Model):
+    description = models.CharField(max_length=200)
+    weight = models.IntegerField(default=0)
+
+
+
 # Create your models here.
