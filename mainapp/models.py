da6de63081e2577f4e3999af15fed1bcd479186c
DC <hemantsureshkumar2012@gmail.com>
2014-11-01 12:00:22 +0800

    comitted to change display syntax "MyModelName object" to "Firstname Lastname"

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-class Product(models.Model):
-    description = models.CharField(max_length=200)
-    weight = models.IntegerField(default=0)
-
 class Customer(models.Model):
     firstname =models.CharField(max_length=100)
     familyname = models.CharField(max_length=100)
@@ -11,6 +7,15 @@ class Customer(models.Model):
     zipcode = models.IntegerField(default=0)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '%s %s' % (self.firstname,self.familyname)
+
+
+class Product(models.Model):
+    description = models.CharField(max_length=200)
+    weight = models.IntegerField(default=0)
+
 
 #s
 # Create your models here.
