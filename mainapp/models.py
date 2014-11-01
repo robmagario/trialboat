b6ed43636c18c4000a10629d19858c158475f98f
DC <hemantsureshkumar2012@gmail.com>
2014-11-01 12:48:01 +0800

    added fields for username and password

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Customer(models.Model):
-    firstname =models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
     familyname = models.CharField(max_length=100)
-    address=models.CharField(max_length=200)
+    address= models.CharField(max_length=200)
     zipcode = models.IntegerField(default=0)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    username = models.CharField(max_length=12)
-    password = models.CharField(max_length=12)
+    email = models.CharField(max_length=100, default=0)
+    username = models.CharField(max_length=1, default=0)
+    password = models.CharField(max_length=12, default=0)
 
     def __str__(self):
         return '%s %s' % (self.firstname,self.familyname)
