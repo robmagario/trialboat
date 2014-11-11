2b857782c4f037d6be409b57b8130142e7fcac34
Robson HayashidaE <robson@magario.com>
2014-11-11 17:16:59 +0800

    changed zip to become char instead of integer

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,12 +5,12 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     family_name = models.CharField(max_length=100)
     address= models.CharField(max_length=200)
-    zip = models.IntegerField(default=0)
+    zip = models.CharField(max_length=10)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     username = models.CharField(max_length=12)
-    password = models.CharField(max_length=12)
+    password = models.CharField(max_length=20)
 
     def __str__(self):
         return '%s %s' % (self.firstname,self.familyname)
