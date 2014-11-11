e8b3c64caaf01705a945ce4eb460b31af57ae827
Kenta Iwasaki <dranithix@gmail.com>
2014-11-11 09:55:46 +0800

    Did some basic login authentication and made a base for the home page.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 @login_required
-def home(request):
-  return HttpResponse('Home Page')
+class HomeView(TemplateView):
+    template_name = "home.html"
--- trialboat/urls.py
+++ trialboat/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.auth.views import login
-from django.contrib import auth
+
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,5 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'django.contrib.auth.views.login')
+    url(r'^home/', TemplateView.as_view(template_name="home.html"), name='home'),
+    url(r'^$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html'}, name='auth')
+
     )
