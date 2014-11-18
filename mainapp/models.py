8358da66eee00dd6882762d5886e5af266863533
DC <hemantsureshkumar2012@gmail.com>
2014-11-18 18:06:01 +0800

    added order models

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django import forms
 
 
-
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     family_name = models.CharField(max_length=100)
@@ -15,7 +14,7 @@ class Customer(models.Model):
     password = models.CharField(max_length=20)
 
     def __str__(self):
-        return '%s %s' % (self.first_name,self.family_name)
+        return '%s %s' % (self.first_name , self.family_name)
 
 
 class Product(models.Model):
@@ -27,9 +26,11 @@ class Product(models.Model):
         return self.description
 
 class Order(models.Model):
-
     user = models.ForeignKey(Customer)
     address_line = models.CharField(max_length=1000)
     order_weight = models.IntegerField(max_length=1000)
     price = models.IntegerField(max_length=1000)
+
+    def __str__(self):
+            return self.address_line
 
