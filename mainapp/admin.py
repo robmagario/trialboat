c87789bfdbfe7d5f1ded98a96ff494090fbbb66a
DC <hemantsureshkumar2012@gmail.com>
2014-11-27 18:31:27 +0800

    Fixed some model stuff.

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from mainapp.models import Product
 from mainapp.models import Customer
 from mainapp.models import Order
+from mainapp.models import Country
 
 admin.site.register(Product)
 admin.site.register(Customer)
 admin.site.register(Order)
+admin.site.register(Country)
 # Register your models here.
