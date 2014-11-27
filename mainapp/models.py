cd870af2a5f7a5b2fda87ce2a5ec29014571928a
DC <hemantsureshkumar2012@gmail.com>
2014-11-27 18:19:42 +0800

    added order models

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,33 +2,19 @@ from django.db import models
 from django import forms
 
 
-class Country(models.Model):
-    COUNTRIES = (
-        ('BR', 'Brazil'),
-        ('JP', 'Japan'),
-        ('PT', 'Portugal'),
-        ('US', 'United States'),
-    )
-    country = models.CharField(max_length=2, choices=COUNTRIES,default='BR')
-
-    def __str__(self):
-        return self.country
-
-
-
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     family_name = models.CharField(max_length=100)
     address= models.CharField(max_length=200)
     zip = models.CharField(max_length=10)
     city = models.CharField(max_length=100)
-    country = models.ForeignKey(Country)
+    country = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     username = models.CharField(max_length=12)
     password = models.CharField(max_length=20)
 
     def __str__(self):
-        return '%s %s' % (self.first_name , self.family_name)
+        return '%s %s' % (self.first_name, self.family_name)
 
 
 class Product(models.Model):
@@ -40,7 +26,8 @@ class Product(models.Model):
         return self.description
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer)
+    user = models.ForeignKey(Customer)
+    address_line = models.CharField(max_length=1000)
     order_weight = models.IntegerField(max_length=1000)
     price = models.IntegerField(max_length=1000)
 
