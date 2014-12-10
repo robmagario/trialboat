dfbc33f01be3bf525cf8e83ac6c229ca0f5813e8
Robson HayashidaE <robson@magario.com>
2014-12-10 18:23:18 +0800

    added registration page.

---
+++ mainapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+from mainapp.models import Customer
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Customer
