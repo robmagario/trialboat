2b497bc720384aabe13321b96995f47cc5f1eb02
Kenta Iwasaki <dranithix@gmail.com>
2014-12-13 15:47:14 +0800

    Added order method w/ index checks.

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,63 +8,33 @@ from mainapp.forms import UserForm, UserProfileForm, SelectProductForm
 @login_required
 def home(request):
     products = models.Product.objects.filter(customer=models.Customer.objects.get(user=request.user))
-    print(products)
-    context = {'form': SelectProductForm(products)}
+    if (request.method == 'POST'):
+        form = SelectProductForm(products, data=request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = SelectProductForm(products)
+    context = {'form': form}
     return render(request, "home.html", context)
 
 
-    # def register(request):
-    #   context = {}
-    #   return render(request, "register.html", context)
-
 
 def register(request):
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
     registered = False
-
-    # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
-
-        # If the two forms are valid...
         if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data to the database.
             user = user_form.save()
-
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
             user.set_password(user.password)
             user.save()
-
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves, we set commit=False.
-            # This delays saving the model until we're ready to avoid integrity problems.
             profile = profile_form.save(commit=False)
             profile.user = user
-
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and put it in the UserProfile model.
-            # if 'picture' in request.FILES:
-            #profile.picture = request.FILES['picture']
-
-            # Now we save the UserProfile model instance.
             profile.save()
-
-            # Update our variable to tell the template registration was successful.
             registered = True
 
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
         else:
             print(user_form.errors, profile_form.errors)
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
